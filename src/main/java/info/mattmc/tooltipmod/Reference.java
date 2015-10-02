d8ca2465f15c7b9aa6c022f3316985ca3fb0e9e6
Aroma1997 <aroma1997@aroma1997.org>
2015-10-02 10:34:07 +0200

    Main mod files.

---
+++ src/main/java/info/mattmc/tooltipmod/Reference.java
@@ -0,0 +1,34 @@
+package info.mattmc.tooltipmod;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+import com.google.common.base.Throwables;
+
+public class Reference {
+	
+	private static String getProp(String id) {
+		if (prop == null) {
+			try {
+				prop = new Properties();
+				InputStream stream = Reference.class
+						.getResourceAsStream("reference.properties");
+				prop.load(stream);
+				stream.close();
+			} catch (Exception e) {
+				prop = null;
+				Throwables.propagate(e);
+			}
+		}
+		return prop.getProperty(id);
+	}
+
+	private static Properties prop;
+	
+	public static final String MOD_ID = "TooltipMod";
+	
+	public static final String MOD_NAME = MOD_ID;
+	
+	public static final String VERSION = getProp("version");
+
+}
---
+++ src/main/java/info/mattmc/tooltipmod/TooltipMod.java
@@ -0,0 +1,29 @@
+package info.mattmc.tooltipmod;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME)
+public class TooltipMod {
+	
+	@Instance
+	public static TooltipMod instance;
+	
+	@SidedProxy(clientSide = "info.mattmc.tooltipmod.client.ClientProxy", serverSide = "info.mattmc.tooltipmod.CommonProxy")
+	public static CommonProxy proxy;
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		event.getModMetadata().version = Reference.VERSION;
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		
+	}
+
+}
