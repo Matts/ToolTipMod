2bb89371042aeae58fccd3609a20fee83ebe16cd
Aroma1997 <aroma1997@aroma1997.org>
2016-05-18 22:07:46 +0200

    Relicense.

--- src/main/java/info/mattmc/tooltipmod/client/ClientProxy.java
+++ src/main/java/info/mattmc/tooltipmod/client/ClientProxy.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2016, MattsOnMc and Aroma1997
+ *
+ * The Tooltipmod is distributed under the terms of the Minecraft Mod Public
+ * License 1.0, or MMPL. Please check the contents of the license located in
+ * http://www.mod-buildcraft.com/MMPL-1.0.txt
+ */
 package info.mattmc.tooltipmod.client;
 
 import info.mattmc.tooltipmod.CommonProxy;
