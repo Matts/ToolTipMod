07c23744baa140f1c618d3b484bc539d88995fe6
Matt <matthewsmeets@gmail.com>
2017-10-03 13:30:56 +0200

    Replaced dangling javadoc with block comment

--- src/main/java/com/mattsmeets/tooltipmod/TooltipHelper.java
+++ src/main/java/com/mattsmeets/tooltipmod/TooltipHelper.java
@@ -1,11 +1,10 @@
-/**
- * Copyright (c) 2016, MattsOnMc and Aroma1997
- * <p>
- * The Tooltipmod is distributed under the terms of the Minecraft Mod Public
- * License 1.0, or MMPL. Please check the contents of the license located in
- * http://www.mod-buildcraft.com/MMPL-1.0.txt
+/*
+  Copyright (c) 2016 / 2017, MattsOnMc and Aroma1997
+  <p>
+  The Tooltipmod is distributed under the terms of the Minecraft Mod Public
+  License 1.0, or MMPL. Please check the contents of the license located in
+  http://www.mod-buildcraft.com/MMPL-1.0.txt
  */
-
 package com.mattsmeets.tooltipmod;
 
 import java.util.ArrayList;
